2fb5e017145f5e53aeafd6de54b386dc62697037
Frn-sz <fernandogdemedeirosw@gmail.com>
2025-02-24 15:12:02 -0300

    feat: process create ATA ix

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,14 +1,31 @@
-pub enum Instruction {
+pub enum AtaInstructions {
     CreateATA(CreateAta),
 }
 
 pub struct CreateAta {
     pub discriminator: u8,
-    mint: [u8; 32],
+    pub mint: [u8; 32],
+    pub wallet: [u8; 32],
 }
 
-impl Instruction {
-    pub fn unpack(input: &) {
+impl AtaInstructions {
+    pub fn unpack(input: &[u8]) -> Result<AtaInstructions, ()> {
+        let (disc, rest) = input.split_first().unwrap();
 
+        match disc {
+            0 => {
+                let mint: [u8; 32] = rest[0..32].try_into().unwrap();
+                let wallet: [u8; 32] = rest[32..64].try_into().unwrap();
+
+                let create_ata = CreateAta {
+                    discriminator: *disc,
+                    mint,
+                    wallet,
+                };
+
+                Ok(AtaInstructions::CreateATA(create_ata))
+            }
+            _ => Err(()),
+        }
     }
 }
--- src/processor.rs
+++ src/processor.rs
@@ -1,37 +1,67 @@
-use std::io::Read;
-
 use pinocchio::{
     account_info::AccountInfo,
     instruction::{AccountMeta, Instruction},
+    msg,
+    program::invoke,
     pubkey::find_program_address,
 };
 
-use crate::constants::{
-    SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID, SPL_TOKEN_PROGRAM_ID, SYSTEM_PROGRAM_ID,
+use crate::{
+    constants::{SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID, SPL_TOKEN_PROGRAM_ID, SYSTEM_PROGRAM_ID},
+    instructions::{AtaInstructions, CreateAta},
 };
 
 pub struct Processor {}
 
 impl Processor {
-    pub fn process_create_ata(
+    pub fn process_instruction(
         program_id: &[u8; 32],
         accounts: &[AccountInfo],
         instruction_data: &[u8],
     ) {
-        let ata = Self::derive_ata(wallet, mint);
+        let ix_data = AtaInstructions::unpack(instruction_data);
+
+        match ix_data {
+            Ok(AtaInstructions::CreateATA(create_ata)) => {
+                Self::process_create_ata(&create_ata, accounts);
+            }
+            Err(_) => {}
+        }
+    }
+    fn process_create_ata(create_ata: &CreateAta, accounts: &[AccountInfo]) {
+        let ata = Self::derive_ata(&create_ata.wallet, &create_ata.mint);
 
         let ix = Instruction {
             program_id: &SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
             data: &[],
             accounts: &[
-                AccountMeta::new(payer, true, true),
+                AccountMeta::new(&create_ata.wallet, true, true),
                 AccountMeta::new(&ata, true, false),
-                AccountMeta::readonly(wallet),
-                AccountMeta::readonly(mint),
+                AccountMeta::readonly(&create_ata.wallet),
+                AccountMeta::readonly(&create_ata.mint),
                 AccountMeta::readonly(&SYSTEM_PROGRAM_ID),
                 AccountMeta::readonly(&SPL_TOKEN_PROGRAM_ID),
             ],
         };
+        let mut acc_iter = accounts.iter();
+
+        let accounts = [
+            acc_iter.next().unwrap(),
+            acc_iter.next().unwrap(),
+            acc_iter.next().unwrap(),
+            acc_iter.next().unwrap(),
+            acc_iter.next().unwrap(),
+            acc_iter.next().unwrap(),
+        ];
+
+        let res = invoke(&ix, &accounts);
+
+        match res {
+            Ok(_) => {
+                msg!("Associated token account created successfully");
+            }
+            Err(_) => {}
+        }
     }
 
     fn derive_ata(wallet: &[u8; 32], mint: &[u8; 32]) -> [u8; 32] {
